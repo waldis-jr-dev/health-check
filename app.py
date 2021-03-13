08b326f27b445257eeb2eaf92bdb79cab76d435f
Waldis <wjr@waldisjr.com>
2021-03-13 10:05:25 +0200

    add logo

--- app.py
+++ app.py
@@ -12,11 +12,17 @@ def page_not_found(e):
 def index():
     if request.method == 'GET':
         return render_template('index.html')
-    if request.method == 'GET':
-        if ['weight', 'height', 'waist', 'sex'] in request.form:
-            return render_template('resp.html')
+    if request.method == 'POST':
+        if 'weight' in request.form and 'height' in request.form \
+                and 'hip_girth' in request.form and 'sex' in request.form:
+            return render_template('resp.html',
+                                   imt=(int(request.form['weight']) /
+                                       (int(request.form['height'])/100) ** 2),
+                                   hip_girth_to_height=int(request.form['hip_girth']) /
+                                                       int(request.form['height']) * 100,
+                                   sex=request.form['sex'])
+        return render_template('index.html')
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
